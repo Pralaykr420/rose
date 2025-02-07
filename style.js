11f22feb47707b3e4e8d701e274e75f072ce2b3c
Pralay Kumar Mahata <147915906+Pralaykr420@users.noreply.github.com>
2025-02-07 16:35:52 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const popup = document.getElementById('popup');
+
+    setTimeout(() => {
+        popup.style.display = 'block';
+    }, 1000);
+});
